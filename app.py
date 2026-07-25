da3f998c8478c197c7e6a7dd4db63fb3372c3db9
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-25 14:08:15 -0300

    feat: adiciona captura da opção escolhida pelo usuário

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ print("Sabor Express\n")
 print("1 - Cadastrar Restaurante")
 print("2 - Listar Restaurante")
 print("3 - Ativar/Desativar Restaurante")
-print("4 - Sair do Programa")
+print("4 - Sair do Programa\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+
+print(f"Você escolheu a opção: {opcao_escolhida}")
