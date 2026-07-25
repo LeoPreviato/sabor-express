1277ae9625319735d758302d2e3bc1e9f0a960d0
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-25 18:30:01 -0300

    feat: adiciona finalização do app com limpeza do terminal

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -14,6 +16,10 @@ print("4 - Sair do Programa\n")
 
 opcao_escolhida = int(input("Escolha uma opção: "))
 
+def finalizar_app():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Finalizando o app\n")
+
 if opcao_escolhida == 1:
     print("Cadastrar restaurante")
 elif opcao_escolhida == 2:
@@ -21,4 +27,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print("Ativar/Desativar restaurante")
 else:
-    print("Encerrando programa")
+    finalizar_app()
