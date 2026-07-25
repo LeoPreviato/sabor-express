6c04bec770af3a60295300aa90ed5fd80f3c8df6
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-25 15:09:10 -0300

    fix: converte opção escolhida para inteiro

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ print("2 - Listar Restaurantes")
 print("3 - Ativar/Desativar Restaurante")
 print("4 - Sair do Programa\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
+opcao_escolhida = int(input("Escolha uma opção: "))
 
 if opcao_escolhida == 1:
     print("Cadastrar restaurante")
