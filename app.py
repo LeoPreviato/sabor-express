10868749c410933ecbb689be8a29d7e51fcb9908
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-26 09:47:06 -0300

    refactor: organiza fluxo do app em funções

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-print("""
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -9,22 +10,32 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░      
 """)
 
-print("1 - Cadastrar Restaurante")
-print("2 - Listar Restaurantes")
-print("3 - Ativar/Desativar Restaurante")
-print("4 - Sair do Programa\n")
-
-opcao_escolhida = int(input("Escolha uma opção: "))
+def exibir_opcoes():
+    print("1 - Cadastrar Restaurante")
+    print("2 - Listar Restaurantes")
+    print("3 - Ativar/Desativar Restaurante")
+    print("4 - Sair do Programa\n")
 
 def finalizar_app():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Finalizando o app\n")
+    print("\nFinalizando o app\n")
 
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes")
-elif opcao_escolhida == 3:
-    print("Ativar/Desativar restaurante")
-else:
-    finalizar_app()
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar/Desativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
