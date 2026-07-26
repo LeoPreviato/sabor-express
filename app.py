2c0f24a6a7f62a15b0962ec01aa78f38362bd767
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-26 10:32:40 -0300

    feat: adiciona função para tratar opções inválidas
    Cria a função opcao_invalida para exibir uma mensagem de erro e retornar ao menu principal. Também adiciona try/except na escolha da opção para lidar com entradas que não podem ser convertidas para número.

--- app.py
+++ app.py
@@ -19,22 +19,31 @@ def exibir_opcoes():
 def finalizar_app():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Finalizando o app\n")
+    
+def opcao_invalida():
+    print("Opção inválida\n")
+    input("Digite uma tecla para voltar ao menu principal")
+    main()
 
 def escolher_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
 
-    if opcao_escolhida == 1:
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3:
-        print("Ativar/Desativar restaurante")
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
-        print("Opção inválida")
+        if opcao_escolhida == 1:
+            print("Cadastrar restaurante")
+        elif opcao_escolhida == 2:
+            print("Listar restaurantes")
+        elif opcao_escolhida == 3:
+            print("Ativar/Desativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
